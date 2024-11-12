29506946e1e5a86c9ff3262edcc98c089e7a7f1f
Shruti18bhatia <shrutibhatia1811@gmail.com>
2024-11-12 15:40:03 +0530

    day9

--- Day8/react-app/src/App.jsx
+++ Day8/react-app/src/App.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import './App.css'
+//rafce
 
-const App = () => {
+const App = ({name,email,mob}) => {
   return (
-    <div>App</div>
+    <div id="disp">
+      <h1>Student Details</h1>
+      <h2>Name: {name}</h2>
+      <h2>Email Id: {email}</h2>
+      <h2>Mobile No: {mob}</h2>
+    </div>
   )
 }
 
--- Day8/react-app/src/main.jsx
+++ Day8/react-app/src/main.jsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.jsx'
+import Marks from './Marks.jsx'
+import DisplayMarks from './DisplayMarks.jsx'
+
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <App name={"Shivani"} email={"shivani@gmail.com"} mob={"90XXXXX"}/>
+    <DisplayMarks/>
   </StrictMode>,
 )
